abd2475cbc35629695b13a1ba914996ddf7c1502
Sean <kejistan@gmail.com>
2013-04-15 23:42:23 -0500

    Started initialization of motif finding algorithm, removed copiled files

--- findMotif.py
+++ findMotif.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 
-import argparse, random
+import argparse, random, numpy
+from pyfasta import Fasta
+
+class MotifFinder:
+	def __init__(self, sequences, motif_length):
+		self.motif_length = motif_length
+		self.sequences = sequences
+		self.sequence_count = len(sequences.keys())
+		self.residue_frequencies = numpy.zeros((motif_length, 20))
+		self.background_frequenceis = numpy.zeros(20)
+		self.alignments = numpy.zeros(self.sequence_count)
+
 
 def main():
 	parser = argparse.ArgumentParser(description='Motif finding using the Gibbs Motif Sampling Algorithm', formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -11,6 +22,11 @@ def main():
 	parser.add_argument('length', type=file, help='Path to the file containing the motif length')
 	parser.add_argument('--prefix', type=str, help='Prefix output files with this string', default='')
 	args = parser.parse_args()
+
+	motif_length = int(args.length.readline())
+	sequences = Fasta(args.sequences)
+
+	finder = MotifFinder(sequences, motif_length)
 
 if __name__ == '__main__':
 	main()
